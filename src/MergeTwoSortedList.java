12b44716347c0c955539491d616ce44fd0451b98
陈玎奥 <chendingao@meituan.com>
2019-06-01 23:38:41 +0800

    Update src/MergeTwoSortedList.java

---
+++ src/MergeTwoSortedList.java
@@ -0,0 +1,49 @@
+/**
+ * MergeTwoSortedList
+ *
+ * @author black
+ * @date 2019/6/1
+ */
+public class MergeTwoSortedList {
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode(int x) { val = x; }
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode node = null;
+        ListNode now = null;
+        while (l1 != null || l2 != null) {
+            ListNode recentNode;
+            if (l1 == null) {
+                recentNode = l2;
+                l2 = l2.next;
+            } else if (l2 == null) {
+                recentNode = l1;
+                l1 = l1.next;
+            } else {
+                if (l1.val > l2.val) {
+                    recentNode = l2;
+                    l2 = l2.next;
+                } else {
+                    recentNode = l1;
+                    l1 = l1.next;
+                }
+            }
+            if (node == null) {
+                node = recentNode;
+                now = node;
+            } else {
+                now.next = recentNode;
+                now = recentNode;
+            }
+        }
+        return node;
+    }
+}

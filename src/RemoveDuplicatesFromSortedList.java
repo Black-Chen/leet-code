3f7fc9a9daa265ed425511f5cdf5a34f3acbbbc8
陈玎奥 <chendingao@meituan.com>
2019-06-03 16:47:58 +0800

    Create RemoveDuplicatesFromSortedList.java

---
+++ src/RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,29 @@
+/**
+ * RemoveDuplicatesFromSortedList
+ *
+ * @author black
+ * @date 2019/6/3
+ */
+public class RemoveDuplicatesFromSortedList {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static MergeTwoSortedList.ListNode deleteDuplicates(MergeTwoSortedList.ListNode head) {
+        MergeTwoSortedList.ListNode node = head;
+        if (head != null) {
+            int value = head.val;
+            while (head.next != null) {
+                if (value == head.next.val) {
+                    head.next = head.next.next;
+                    continue;
+                } else {
+                    value = head.next.val;
+                    head = head.next;
+                }
+            }
+        }
+        return node;
+    }
+}

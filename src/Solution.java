1412a563507702d2649f53eb14a32a96cf75ba2c
陈玎奥 <chendingao@meituan.com>
2019-07-19 17:47:42 +0800

    Update Solution.java

--- src/Solution.java
+++ src/Solution.java
@@ -7,6 +7,7 @@ import java.math.BigInteger;
  * @date 2019-07-10
  */
 class Solution {
+    // 两个正整数范围[1-1000000000]相乘的结果中，有多少个二进制的1
     public static int solution(int A, int B) {
         // write your code in Java SE 8
         BigInteger multiResult = BigInteger.valueOf(A).multiply(BigInteger.valueOf(B));
@@ -22,6 +23,7 @@ class Solution {
         return count;
     }
 
+    //找出字符串中出现评率最高的英文字母，只有小写字母，当有相同频率的字母时，按照字典序返回第一个
     public static String rawSolution(String S) {
         int[] occurrences = new int[26];
         for (char ch : S.toCharArray()) {

85674016f4a1e4d4ebc7ebc676cf8368a43b13ac
陈玎奥 <chendingao@meituan.com>
2019-07-24 11:59:51 +0800

    Update Pow.java
    optimization

--- src/medium/Pow.java
+++ src/medium/Pow.java
@@ -9,7 +9,7 @@ package medium;
 public class Pow {
 
     public static void main(String[] args) {
-        System.out.println(Math.pow(2, -3) == myPow(2, -3));
+        System.out.println(Math.pow(2, Integer.MIN_VALUE) == myPow(2, Integer.MIN_VALUE));
     }
 
     public static double myPow(double x, int n) {
@@ -21,13 +21,14 @@ public class Pow {
         }
         if (x == -1.0d) {
             int remain = n % 2;
-            return remain > 0 ? -x : x;
+            return remain > 0 ? x : -x;
         }
+        long value = n;
         if (n < 0) {
             x = 1/x;
-            n = -n;
+            value = -value;
         }
-        return pow(x, n - 1, x);
+        return powHalf(x, value);
     }
 
     private static double pow(double x, int n, double lastResult) {
@@ -36,5 +37,14 @@ public class Pow {
             result = pow(x, n - 1, lastResult * x);
         }
         return result;
+    }
+
+    private static double powHalf(double x, long n) {
+        if (n == 0) {
+            return 1;
+        }
+        double half = powHalf(x, n / 2);
+        double remainder = n % 2 > 0 ? x : 1;
+        return half * half * remainder;
     }
 }

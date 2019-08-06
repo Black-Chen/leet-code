8275427daed0a8614e48293e1a2318c945b11f24
陈玎奥 <chendingao@meituan.com>
2019-08-06 16:16:05 +0800

    Update HammingDistance.java
    1.降低代码复杂度

--- src/easy/HammingDistance.java
+++ src/easy/HammingDistance.java
@@ -15,6 +15,18 @@ public class HammingDistance {
         System.out.println(hammingDistanceNew(1, 4));
     }
 
+    public static int hammingDistanceNew1(int x, int y) {
+        int count = 0;
+        while (x != 0 || y != 0) {
+            if ((x & 1) != (y & 1)) {
+                count++;
+            }
+            x = x >> 1;
+            y = y >> 1;
+        }
+        return count;
+    }
+
     public static int hammingDistanceNew(int x, int y) {
         int count = 0;
         while (x != 0 || y != 0) {

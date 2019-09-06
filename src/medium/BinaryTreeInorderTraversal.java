c6f5b7bd5ff77a1bf944d5e09a0d0fe17209130d
cda332978465@gmail.com <chendingao@meituan.com>
2019-09-06 15:23:13 +0800

    Create BinaryTreeInorderTraversal.java

---
+++ src/medium/BinaryTreeInorderTraversal.java
@@ -0,0 +1,41 @@
+package medium;
+
+import easy.IsSameTree.TreeNode;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * BinaryTreeInorderTraversal
+ *
+ * @author black
+ * @date 2019-09-06
+ */
+public class BinaryTreeInorderTraversal {
+
+    public static void main(String[] args) {
+        TreeNode a = new TreeNode(2);
+        TreeNode b = new TreeNode(3);
+        TreeNode c = new TreeNode(1);
+        a.left = b;
+        b.left = c;
+        System.out.println(inorderTraversal(a));
+    }
+
+    public static List<Integer> inorderTraversal(TreeNode root) {
+        Stack<TreeNode> stack = new Stack<>();
+        List<Integer> result = new ArrayList<>();
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            result.add(root.val);
+            root = root.right;
+        }
+        return result;
+    }
+}

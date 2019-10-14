2b455848fa655a4ae246b8c66201eda3356ced1d
陈玎奥 <chendingao@meituan.com>
2019-10-14 15:08:39 +0800

    update

--- src/medium/BinaryTreeZigzagLevelOrderTraversal.java
+++ src/medium/BinaryTreeZigzagLevelOrderTraversal.java
@@ -3,6 +3,7 @@ package medium;
 import easy.IsSameTree.TreeNode;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 /**
@@ -16,6 +17,8 @@ public class BinaryTreeZigzagLevelOrderTraversal {
     public static void main(String[] args) {
 
     }
+
+
 
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
         List<List<Integer>> result = new ArrayList<>();
@@ -57,5 +60,66 @@ public class BinaryTreeZigzagLevelOrderTraversal {
             result.add(node);
         }
         return result;
+    }
+
+    public List<List<Integer>> zigzagLevelOrder2(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        boolean flag = true;
+        List<TreeNode> lastNode = new ArrayList<>();
+        List<TreeNode> currentNode = new ArrayList<>();
+        LinkedList<Integer> node;
+        TreeNode parent;
+        if (root != null) {
+            lastNode.add(root);
+        }
+        while (!lastNode.isEmpty()) {
+            node = new LinkedList<>();
+            for (int i = 0; i < lastNode.size(); i++) {
+                parent = lastNode.get(i);
+                if (flag) {
+                    node.add(parent.val);
+                } else {
+                    node.addFirst(parent.val);
+                }
+                if (parent.left != null) {
+                    currentNode.add(parent.left);
+                }
+                if (parent.right != null) {
+                    currentNode.add(parent.right);
+                }
+
+            }
+            lastNode = currentNode;
+            currentNode = new ArrayList<>();
+            flag = !flag;
+            result.add(node);
+        }
+        return result;
+    }
+
+    public List<List<Integer>> zigzagLevelOrder3(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        dfs(root, 0, result);
+        return result;
+    }
+
+    private void dfs(TreeNode root, int depth, List<List<Integer>> result) {
+        if (root == null) {
+            return;
+        }
+
+        if (depth == result.size()) {
+            result.add(new LinkedList<>());
+        }
+
+        LinkedList<Integer> list = (LinkedList<Integer>) result.get(depth);
+        if ((depth & 1) == 0) {
+            list.add(root.val);
+        } else {
+            list.addFirst(root.val);
+        }
+
+        dfs(root.left, depth + 1, result);
+        dfs(root.right, depth + 1, result);
     }
 }
